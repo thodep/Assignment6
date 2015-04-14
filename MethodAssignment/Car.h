673f5d161e3cd82cb5233330608cc609395fb4c8
thodep <thodep@yahoo.com>
2015-04-13 22:08:32 -0400

    Finish Method Assignment

--- MethodAssignment/Car.h
+++ MethodAssignment/Car.h
@@ -10,40 +10,48 @@
 
 @interface Car : NSObject
 
+@property (copy) NSString *model;
+
+
+- (void)startEngine;
+- (void)driveForDistance:(NSNumber *)theDistance;
+- (void)turnByAngle:(NSNumber *)theAngle
+            quickly:(NSNumber *)useParkingBrake;
 //Accessors
--(BOOL)isRunning;
--(void)setRunning:(BOOL)running;
--(NSString *)model;
--(void)setModel:(NSString *)model;
+//-(BOOL)isRunning;
+//-(void)setRunning:(BOOL)running;
+//-(NSString *)model;
+//-(void)setModel:(NSString *)model;
 
 //Calculated values
 
--(double)maximumSpeed;
--(double)maximumSpeedUsingLocale:(NSLocale *)locale;
+//-(double)maximumSpeed;
+//-(double)maximumSpeedUsingLocale:(NSLocale *)locale;
 
 //Action Methods
--(void)startEngine;
--(void)driveForDistance:(id)theOrigin toDestination: (id)theDestination;
--(void)turnByAngle :(double)theAngle;
--(void)turnToAngle:(double)theAngle;
+//-(void)startEngine;
+//-(void)driveForDistance:(id)theOrigin toDestination: (id)theDestination;
+//-(void)turnByAngle :(double)theAngle;
+//-(void)turnToAngle:(double)theAngle;
 
 //Error handling methods
 
--(BOOL)loadPassenger:(id)aPassenger error:(NSError **)error;
+//-(BOOL)loadPassenger:(id)aPassenger error:(NSError **)error;
 //Constructor methods
--(id)initWithModel:(NSString *)amodel;
--(id)initWithModel:(NSString *)amodel mileage:(double)theMileage;
+//-(id)initWithModel:(NSString *)amodel;
+//-(id)initWithModel:(NSString *)amodel mileage:(double)theMileage;
+
 //Comparison methods
--(BOOL)isEqualToCar:(Car *)anotherCar;
--(Car *)fasterCar:(Car *)anotherCar;
--(Car *)slowerCar:(Car *)anotherCar;
+//-(BOOL)isEqualToCar:(Car *)anotherCar;
+//-(Car *)fasterCar:(Car *)anotherCar;
+//-(Car *)slowerCar:(Car *)anotherCar;
 
 //Factory methods
-+(Car *)car;
-+(Car *)carWithModel:(NSString *)aModel;
-+(Car *)carWithModel:(NSString *)aModel mileage :(double)theMileage;
+//+(Car *)car;
+//+(Car *)carWithModel:(NSString *)aModel;
+//+(Car *)carWithModel:(NSString *)aModel mileage :(double)theMileage;
 //Singletonn methods
-+(Car *)shareCar;
+//+(Car *)shareCar;
 
 
 
--- MethodAssignment/Car.m
+++ MethodAssignment/Car.m
@@ -8,8 +8,28 @@
 
 #import "Car.h"
 
-@implementation Car {
-    double _odometer;
-    
+@implementation Car
+// Selectors method code
+
+@synthesize model = _model;
+
+- (void)startEngine {
+    NSLog(@"Starting the %@'s engine", _model);
+}
+
+- (void)driveForDistance:(NSNumber *)theDistance {
+    NSLog(@"The %@ just drove %0.1f miles",
+          _model, [theDistance doubleValue]);
+}
+
+- (void)turnByAngle:(NSNumber *)theAngle
+            quickly:(NSNumber *)useParkingBrake {
+    if ([useParkingBrake boolValue]) {
+        NSLog(@"The %@ is drifting around the corner!", _model);
+    } else {
+        NSLog(@"The %@ is making a gentle %0.1f degree turn",
+              _model, [theAngle doubleValue]);
+    }
+}
 
 @end
